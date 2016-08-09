aafb15c3a4c705d7548f9758c73e254abd4abce4
gongthub <1056685839@qq.com>
2016-08-09 13:55:44 +0800

    Add

---
+++ Project/HelloWord/src/com/example/helloword/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.helloword;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// 获取测试按钮
+		final Button btnTest = (Button) findViewById(R.id.btnTest);
+
+		// 设置测试按钮点击事件
+		btnTest.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				btnTest.setText("hello");
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
